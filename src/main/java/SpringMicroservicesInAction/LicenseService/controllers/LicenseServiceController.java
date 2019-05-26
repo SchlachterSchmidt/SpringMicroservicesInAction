f71911da2d6548227e9daacdc8094e63526da278
SchlachterSchmidt <teenageenemanursesinbondage@gmail.com>
2019-05-26 23:18:25 +0100

    chapter 2 code of Spring Microservices In Action

---
+++ src/main/java/SpringMicroservicesInAction/LicenseService/controllers/LicenseServiceController.java
@@ -0,0 +1,26 @@
+package SpringMicroservicesInAction.LicenseService.controllers;
+
+import SpringMicroservicesInAction.LicenseService.models.License;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
+
+@RestController
+@RequestMapping(value="/v1/organizations/{organizationId}/licenses")
+public class LicenseServiceController {
+
+    @RequestMapping(value="/{licenseId}", method = GET)
+    public License getLicense(@PathVariable("organizationId") String organizationId,
+                              @PathVariable("licenseId") String licenseId) {
+
+        // initial stump. this will be replaced with call to the service layer akin to:
+        // return service.getLicenseByLicenseIdAndOrganizationId(licenseId, organizationId)
+        return new License()
+                .withLicenseId(licenseId)
+                .withOrganizationId(organizationId)
+                .withProductName("Telco")
+                .withLicenseType("Seat");
+    }
+}
---
+++ src/main/java/SpringMicroservicesInAction/LicenseService/models/License.java
@@ -0,0 +1,61 @@
+package SpringMicroservicesInAction.LicenseService.models;
+
+public class License {
+
+    private String licenseId;
+    private String organizationId;
+    private String productName;
+    private String licenseType;
+
+    public String getLicenseId() {
+        return licenseId;
+    }
+
+    public void setLicenseId(String licenseId) {
+        this.licenseId = licenseId;
+    }
+
+    public String getOrganizationId() {
+        return organizationId;
+    }
+
+    public void setOrganizationId(String organizationId) {
+        this.organizationId = organizationId;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public String getLicenseType() {
+        return licenseType;
+    }
+
+    public void setLicenseType(String licenseType) {
+        this.licenseType = licenseType;
+    }
+
+    public License withOrganizationId(String organizationId) {
+        this.organizationId = organizationId;
+        return this;
+    }
+
+    public License withLicenseId(String licenseId) {
+        this.licenseId = licenseId;
+        return this;
+    }
+
+    public License withProductName(String productName) {
+        this.productName = productName;
+        return this;
+    }
+
+    public License withLicenseType(String licenseType) {
+        this.licenseType = licenseType;
+        return this;
+    }
+}
