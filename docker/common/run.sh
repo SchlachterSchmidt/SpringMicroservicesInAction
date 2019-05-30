832ec9bc973d11c24f86bad013093bb510115931
SchlachterSchmidt <teenageenemanursesinbondage@gmail.com>
2019-05-30 17:52:58 +0100

    adding database connection

--- docker/common/run.sh
+++ docker/common/run.sh
@@ -1,1 +1,18 @@
-#!/usr/bin/env bash
+#!/bin/sh
+
+echo "********************************************************"
+echo "Waiting for the configuration server to start on port $CONFIGSERVER_PORT"
+echo "********************************************************"
+while ! `nc -z configservice $CONFIGSERVER_PORT `; do sleep 3; done
+echo ">>>>>>>>>>>> Configuration Server has started"
+
+echo "********************************************************"
+echo "Waiting for the database server to start on port $DATABASESERVER_PORT"
+echo "********************************************************"
+while ! `nc -z database $DATABASESERVER_PORT`; do sleep 3; done
+echo ">>>>>>>>>>>> Database Server has started"
+
+echo "********************************************************"
+echo "Starting License Server with Configuration Service :  $CONFIGSERVER_URI";
+echo "********************************************************"
+java -Dspring.cloud.config.uri=$CONFIGSERVER_URI -Dspring.profiles.active=$PROFILE -jar /usr/local/licensingservice/LicenseService-0.0.1-SNAPSHOT.jar
