77dc7b023f7ebf70e00562cef75d705bd8045bf0
SchlachterSchmidt <teenageenemanursesinbondage@gmail.com>
2019-06-05 15:20:07 -0500

    removing oauth config from organization service

--- OrganizationService/src/main/kotlin/SpringMicroservicesInAction/OrganizationService/OrganizationServiceApplication.kt
+++ OrganizationService/src/main/kotlin/SpringMicroservicesInAction/OrganizationService/OrganizationServiceApplication.kt
@@ -7,13 +7,13 @@ import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker
 import org.springframework.cloud.client.loadbalancer.LoadBalanced
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient
 import org.springframework.context.annotation.Bean
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer
+//import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer
 import org.springframework.web.client.RestTemplate
 
 @SpringBootApplication
 @EnableEurekaClient
 @EnableCircuitBreaker
-@EnableResourceServer
+//@EnableResourceServer
 class OrganizationServiceApplication {
 
 	@Bean
---
+++ OrganizationService/src/main/kotlin/SpringMicroservicesInAction/OrganizationService/interceptors/RequestLoggingInterceptor.kt
@@ -0,0 +1,34 @@
+package SpringMicroservicesInAction.OrganizationService.interceptors
+
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import org.springframework.stereotype.Component
+import org.springframework.web.servlet.ModelAndView
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter
+import org.springframework.web.util.ContentCachingRequestWrapper
+import org.springframework.web.util.ContentCachingResponseWrapper
+import javax.servlet.http.HttpServletRequest
+import javax.servlet.http.HttpServletResponse
+
+
+@Component
+class RequestLoggingInterceptor : HandlerInterceptorAdapter() {
+
+    val logger: Logger = LoggerFactory.getLogger(RequestLoggingInterceptor::class.java)
+
+    override fun preHandle(request: HttpServletRequest, response: HttpServletResponse, handler: Any): Boolean {
+
+        val cachedRequest: HttpServletRequest = ContentCachingRequestWrapper(request)
+        logger.debug("URI: ${cachedRequest.requestURI}" +
+                "AUTH TYPE: ${cachedRequest.authType}" +
+                "HEADERS: ${cachedRequest.headerNames}" +
+                "PATH INFO: ${cachedRequest.pathInfo}")
+        return super.preHandle(request, response, handler)
+    }
+
+    override fun postHandle(request: HttpServletRequest, response: HttpServletResponse, handler: Any, modelAndView: ModelAndView?) {
+        val cachedResponse: HttpServletResponse = ContentCachingResponseWrapper(response)
+        logger.debug("STATUS: ${cachedResponse.status}")
+        super.postHandle(request, response, handler, modelAndView)
+    }
+}
