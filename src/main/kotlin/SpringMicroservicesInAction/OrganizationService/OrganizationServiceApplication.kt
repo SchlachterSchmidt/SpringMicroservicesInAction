e6699e348a0eecf74a0e9ecbb089df5ba33c9f93
SchlachterSchmidt <teenageenemanursesinbondage@gmail.com>
2019-06-11 00:50:52 +0100

    setting up fallback config for eureka

--- src/main/kotlin/SpringMicroservicesInAction/OrganizationService/OrganizationServiceApplication.kt
+++ src/main/kotlin/SpringMicroservicesInAction/OrganizationService/OrganizationServiceApplication.kt
@@ -2,8 +2,10 @@ package SpringMicroservicesInAction.OrganizationService
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient
 
 @SpringBootApplication
+@EnableEurekaClient
 class OrganizationServiceApplication
 
 fun main(args: Array<String>) {
