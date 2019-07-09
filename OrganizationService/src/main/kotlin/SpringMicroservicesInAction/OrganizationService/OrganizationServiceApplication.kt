758fffa46d83f5ab43178fafc167d8fe0b7d0127
SchlachterSchmidt <teenageenemanursesinbondage@gmail.com>
2019-07-08 22:01:58 -0500

    forwarding correlation id to downstream services

--- OrganizationService/src/main/kotlin/SpringMicroservicesInAction/OrganizationService/OrganizationServiceApplication.kt
+++ OrganizationService/src/main/kotlin/SpringMicroservicesInAction/OrganizationService/OrganizationServiceApplication.kt
@@ -1,14 +1,31 @@
 package SpringMicroservicesInAction.OrganizationService
 
+import SpringMicroservicesInAction.OrganizationService.utils.UserContextInterceptor
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker
+import org.springframework.cloud.client.loadbalancer.LoadBalanced
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient
+import org.springframework.context.annotation.Bean
+import org.springframework.web.client.RestTemplate
 
 @SpringBootApplication
 @EnableEurekaClient
 @EnableCircuitBreaker
-class OrganizationServiceApplication
+class OrganizationServiceApplication {
+
+	@Bean
+	@LoadBalanced
+	fun getRestTemplate(): RestTemplate {
+		val restTemplate = RestTemplate()
+		val interceptors = restTemplate.interceptors
+
+		interceptors.add(UserContextInterceptor())
+		restTemplate.interceptors = interceptors
+
+		return restTemplate
+	}
+}
 
 fun main(args: Array<String>) {
 	runApplication<OrganizationServiceApplication>(*args)
