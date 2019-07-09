2fb9590de7dfddec62bfe98ac88cc278dbbc50dc
SchlachterSchmidt <teenageenemanursesinbondage@gmail.com>
2019-07-08 22:02:35 -0500

    adding simple util scripts to rebuild all apps and tear down docke envr

---
+++ cleandocker.sh
@@ -0,0 +1,13 @@
+docker container rm \
+  springmicroservicesinaction_organizationservice_1 \
+  springmicroservicesinaction_licensingservice_1 \
+  springmicroservicesinaction_gatewayservice_1 \
+  springmicroservicesinaction_eurekaservice_1 \
+  springmicroservicesinaction_configservice_1 \
+
+docker image rm \
+  springmicroservicesinaction_organizationservice \
+  springmicroservicesinaction_licensingservice \
+  springmicroservicesinaction_gatewayservice \
+  springmicroservicesinaction_configservice \
+  springmicroservicesinaction_eurekaservice
---
+++ rebuildall.sh
@@ -0,0 +1,14 @@
+cd ConfigService
+./gradlew clean build
+
+cd ../EurekaService
+./gradlew clean build
+
+cd ../GatewayService
+./gradlew clean build
+
+cd ../LicensingService
+./gradlew clean build
+
+cd ../OrganizationService
+./gradlew clean build
