70b5b3e5df6362d8568931a9fb11e2cc13df0bc2
SchlachterSchmidt <teenageenemanursesinbondage@gmail.com>
2019-07-08 22:00:44 -0500

    adding response filter for outgoing requests

---
+++ GatewayService/src/main/kotlin/SpringMicroservicesInAction/GatewayService/filters/ResponseFilter.kt
@@ -0,0 +1,32 @@
+package SpringMicroservicesInAction.GatewayService.filters
+
+import com.netflix.zuul.ZuulFilter
+import com.netflix.zuul.context.RequestContext
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Component
+
+@Component
+class ResponseFilter : ZuulFilter() {
+
+    @Autowired
+    lateinit var filterUtils: FilterUtils
+
+    val logger: Logger = LoggerFactory.getLogger(ResponseFilter::class.java)
+
+    override fun shouldFilter(): Boolean = true
+
+    override fun filterType(): String = FilterType.POST.type
+
+    override fun filterOrder(): Int = 1
+
+    override fun run() {
+        val context = RequestContext.getCurrentContext()
+
+        logger.debug("Adding the correlation id to the outbound headers: ${filterUtils.getCorrelationId()}")
+        context.response.addHeader(HeaderNames.CORRELATION_ID_HEADER.headerName, filterUtils.getCorrelationId())
+
+        logger.debug("Completing outgoing request for  ${context.request.requestURI}")
+    }
+}
