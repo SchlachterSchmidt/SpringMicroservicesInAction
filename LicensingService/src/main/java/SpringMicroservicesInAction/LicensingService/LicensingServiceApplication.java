f519b505cd0be1ee736c4b523e809fcc818fbd35
SchlachterSchmidt <teenageenemanursesinbondage@gmail.com>
2019-08-23 16:04:06 -0500

    writing kafka message consumer

--- LicensingService/src/main/java/SpringMicroservicesInAction/LicensingService/LicensingServiceApplication.java
+++ LicensingService/src/main/java/SpringMicroservicesInAction/LicensingService/LicensingServiceApplication.java
@@ -1,6 +1,9 @@
 package SpringMicroservicesInAction.LicensingService;
 
+import SpringMicroservicesInAction.LicensingService.events.model.OrganizationChangeModel;
 import SpringMicroservicesInAction.LicensingService.utils.UserContextInterceptor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
@@ -9,6 +12,9 @@ import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.annotation.StreamListener;
+import org.springframework.cloud.stream.messaging.Sink;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
@@ -20,7 +26,10 @@ import java.util.List;
 @EnableFeignClients
 @EnableCircuitBreaker
 @RefreshScope
+@EnableBinding(Sink.class)
 public class LicensingServiceApplication {
+
+	private Logger logger = LoggerFactory.getLogger(this.getClass());
 
 	@LoadBalanced
 	@Bean
@@ -32,6 +41,11 @@ public class LicensingServiceApplication {
 		restTemplate.setInterceptors(interceptors);
 
 		return restTemplate;
+	}
+
+	@StreamListener(Sink.INPUT)
+	public void loggerSink(OrganizationChangeModel organizationChangeModel) {
+		logger.debug("Received event for organization id: {}", organizationChangeModel.getOrganizationId());
 	}
 
 	public static void main(String[] args) {
