336f62887ef69238584cf4a2f647bd28fd3dbc43
SchlachterSchmidt <teenageenemanursesinbondage@gmail.com>
2019-08-25 10:29:42 -0500

    removing commented code

--- LicensingService/src/main/java/SpringMicroservicesInAction/LicensingService/LicensingServiceApplication.java
+++ LicensingService/src/main/java/SpringMicroservicesInAction/LicensingService/LicensingServiceApplication.java
@@ -1,7 +1,6 @@
 package SpringMicroservicesInAction.LicensingService;
 
 import SpringMicroservicesInAction.LicensingService.config.ServiceConfig;
-import SpringMicroservicesInAction.LicensingService.events.model.OrganizationChangeModel;
 import SpringMicroservicesInAction.LicensingService.utils.UserContextInterceptor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -14,9 +13,6 @@ import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.cloud.stream.annotation.EnableBinding;
-import org.springframework.cloud.stream.annotation.StreamListener;
-import org.springframework.cloud.stream.messaging.Sink;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
@@ -31,7 +27,6 @@ import java.util.List;
 @EnableFeignClients
 @EnableCircuitBreaker
 @RefreshScope
-//@EnableBinding(Sink.class)
 public class LicensingServiceApplication {
 
 	private Logger logger = LoggerFactory.getLogger(this.getClass());
@@ -64,11 +59,6 @@ public class LicensingServiceApplication {
 		template.setConnectionFactory(jedisConnectionFactory());
 		return template;
 	}
-
-//	@StreamListener(Sink.INPUT)
-//	public void loggerSink(OrganizationChangeModel organizationChangeModel) {
-//		logger.debug("Received event for organization id: {}", organizationChangeModel.getOrganizationId());
-//	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(LicensingServiceApplication.class, args);
